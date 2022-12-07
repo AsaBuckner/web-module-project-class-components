cc5cafb3ad8517322dd60abdd0e74d2a278a39f8
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2022-12-06 20:44:06 -0600

    flags completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import Form from "./Form"
+import Todo from "./Todo"
+import TodoList from "./TodoList"
 
 export default class App extends React.Component {
-  render() {
+  constructor() {
+    super()
+      this.state = {
+        todos:[
+          {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+          },
+          {
+          name: 'Suck Dick',
+          id: 1528817084358,
+          completed: false
+          },
+          {
+          name: 'Eat Ass',
+          id: 1528817084358,
+          completed: true
+          }
+        ]
+      }
+  }
+
+
+ 
+
+render() {
+  
+  const {todos} = this.state
+
+
+
+
     return (
       <div>
-        Todo App
+          <ui>
+            {todos.map(todo => {
+              return <li>{todo.name} { todo.completed ? <span>Comleted</span>: "" }</li>
+            })}
+          </ui>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-export default class Form extends React.Component {
-  render() {
+class Form extends React.Component {
+  constructor() {
+    super()
+      this.state = {}
+    }
+
+    submitFunction = props => {
+      console.log(submit)
+     }
+     
+     clearFunction = props => {
+       console.log(clear)
+     }
+
+
+  render(){
     return (
       <div>
-        Form
+        <input
+        placeholder='New Todo'
+        name='todoInput'
+        />
+        <button onClick={this.submitFunction}>Add Todo</button>
+        <button onClick={this.clearFunction}>Clear Completed</button>
       </div>
     )
   }
 }
+  export default Form 
